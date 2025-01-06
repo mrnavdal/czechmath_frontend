5da236cc67b4ab5a245141e01079028ecb4d6e40
Dalibor Mrnavek <dalibor.mrnavek@gmail.com>
2025-01-06 13:31:48 +0100

    Přeinstalovaný turbopack na webpack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.devtool = 'source-map'; // Povolení zdrojových map
+    }
+    return config;
+  },
+
+  
 };
 
 export default nextConfig;
